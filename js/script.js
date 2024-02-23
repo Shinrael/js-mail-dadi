77cb979717530bbedea7eee35f2beb7634b76f9e
Shinrael <mircoelena89@gmail.com>
2024-02-23 17:15:30 +0100

    [edit] first exercise

--- js/script.js
+++ js/script.js
@@ -7,18 +7,27 @@ const emailVerificate = ['utente@email.it' , 'utente2@email.it' , 'utente3@email
 // Chiedo la mail all'utente tramite un prompt
 
 const userEmail = prompt ('Inserisci la tua mail');
+
+let allOk = false;
  
 // Controllo se la mail è presente nell'array creato
 
-const verifica = emailVerificate.indexOf(userEmail) !== -1;
+// const verifica = emailVerificate.indexOf(userEmail) !== -1;
+for (let i = 0; i < emailVerificate.length; i++) {
+  const verifica = emailVerificate[i];  
 
+// Verifichiamo se la mail corrisponde alla lista
+  if (userEmail === verifica) {
+    allOk = true;
+  }  
+}
 // Stampiamo un messaggio tramite Alert per l'esito
 
-if (verifica) {
-  alert ('Complimenti è consentito il tuo accesso, Benvenuto!')
-} else {
-  alert ('Mi dispiace, ma non ti è consentito l\accesso')
-}
+  if (allOk) {
+    alert ('Complimenti è consentito il tuo accesso, Benvenuto!')
+  } else {
+    alert ('Mi dispiace, ma non ti è consentito l\accesso')
+  }
 
 // ESERCIZIO 2
 
